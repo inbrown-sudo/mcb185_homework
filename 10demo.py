1cc1f71320b85f2add7f1aa111ebbcfb3681c338
inbrown-sudo <inbrown@ucdavis.edu>
2026-01-19 20:03:50 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -42,6 +42,103 @@ def rectangle_area(w, h): return w * h
 
 def triangle_area(w, h): return rectangle_area(w, h) /2
 
+s = 'hello world'
+print(s, type(s))
+
+a = 2
+b = 2
+if a == b:
+	print('a equals b')
+print(a, b)
+
+def is_even(x):
+	if x % 2 == 0: return True 
+	return False 
+	
+print(is_even(2))
+print(is_even(3))
+
+c = a == b 
+print(c)
+print(type(c))
+
+if a < b:
+	print('a < b')
+elif a > b:
+	print('a > b')
+else:
+	print('a == b')
+
+if   a < b: print('a < b')
+elif a > b: print ('a > b')	
+else:		print('a == b')
+
+if a < b: print('a < b')
+elif a <= b: print('a <= b')
+elif a == b: print('this will never print!')
+
+if a < b or a > b: print('all things being equal, a and b are not')
+if a < b and a > b: print('you are living in a strange world')
+if not False: print(True)
+
+a = 0.3
+b = 0.1 * 3
+if 	 a < b: print('a < b')
+elif a > b: print('a > b')
+else:		print('a == b')
+
+print(abs(a - b)) # 5.551115123125783e-17
+if abs(a - b) < 1e-9: print('close enough')
+
+if math.isclose(a, b): print('close enough')
+
+s1 = 'A'
+s2 = 'B'
+s3 = 'a'
+if s1 < s2: print('A < B')
+if s2 < s3: print('B < a')
+
+# a = 1
+# s = 'G'
+# if a < s: print('a < s')    This results in a Type error
+
+def silly(m, x, b):
+	y = m * x + b
+print(silly(2, 3, 4))
+
+def is_integer(x):			
+	return x % 1 == 0
+	
+def is_probability(p):       
+	return 0 <= p <= 1
+	
+def dna_molecular_weight(base):     
+	base = base.upper()
+	weight = {
+	"A": 313.2, 
+	"C": 289.2, 
+	"G": 329.2, 
+	"T": 304.2
+	}
+	return weight.get(base, None)
+	
+def dna_complement(base):            
+	base = base.upper()
+	complement = {
+	"A": "T", 
+	"T": "A", 
+	"C": "G", 
+	"G": "C"
+	}
+	return complement.get(base, None)     # Come back to these 4 
+	
 
 
+
+
+	
+	
+	
+	
+	
 	
