872b4d4d7865c7c0e3e24e3f3d704efac20615d1
inbrown-sudo <inbrown@ucdavis.edu>
2026-01-22 15:20:15 -0800

    update

--- 13fizzbuzz.py
+++ 13fizzbuzz.py
@@ -1,4 +1,4 @@
-for i in range(1, 100):
+for i in range(1, 101):
 	if i % 3 == 0 and i % 5 == 0: print(i, 'fizzbuzz', end=' ')
 	elif i % 5 == 0: print(i, 'buzz', end=' ')
 	elif i % 3 == 0: print(i, 'fizz', end=' ')
