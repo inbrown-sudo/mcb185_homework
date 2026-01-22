8976b59b998245c42f9c040b62e7c8ece65b0836
inbrown-sudo <inbrown@ucdavis.edu>
2026-01-22 15:27:47 -0800

    update

--- 13fizzbuzz.py
+++ 13fizzbuzz.py
@@ -1,4 +1,5 @@
 for i in range(1, 101):
-	if i % 3 == 0 and i % 5 == 0: print(i, 'fizzbuzz', end=' ')
-	elif i % 5 == 0: print(i, 'buzz', end=' ')
-	elif i % 3 == 0: print(i, 'fizz', end=' ')
+	if i % 3 == 0 and i % 5 == 0: print(i, 'fizzbuzz')
+	elif i % 5 == 0: print(i, 'buzz')
+	elif i % 3 == 0: print(i, 'fizz')
+	else: print(i)
