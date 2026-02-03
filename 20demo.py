cace971fa869bff79544ed1c90ec0a3b309154b6
inbrown-sudo <inbrown@ucdavis.edu>
2026-02-03 15:47:22 -0800

    new

---
+++ 20demo.py
@@ -0,0 +1,24 @@
+# Strings 
+
+s = 'hello world'
+print(s)
+
+s1 = 'hey "dude"'
+s2 = "don't tell me what to do"
+print(s1, s2)
+
+print('hey "dude" don\'t tell me what to do')
+
+print(s.upper())
+print(s)
+
+print(s.replace('o', ''))
+print(s.replace('o', '').replace('r', 'i'))
+
+import math 
+
+print('{} {:.3f}'.format('str.format', math.pi))
+
+print('%s %.3f' % ('printf', math.pi))
+
+# Indexes 
