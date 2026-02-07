09302be702547a3df0568313492fe8a90b1083eb
inbrown-sudo <inbrown@ucdavis.edu>
2026-02-06 21:19:47 -0800

    update

--- 22stats.py
+++ 22stats.py
@@ -5,7 +5,7 @@ import math
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python stats.py <numbers>")
+        print('Usage: python stats.py <numbers>')
         sys.exit(1)
 
     values = [float(x) for x in sys.argv[1:]]
@@ -23,13 +23,13 @@ def main():
     if n % 2 == 1: median = values[n // 2]
     else: median = (values[n // 2 - 1] + values[n // 2]) / 2
 
-    print(f"Count: {n}")
-    print(f"Min: {minimum}")
-    print(f"Max: {maximum}")
-    print(f"Mean: {mean}")
-    print(f"Standard Deviation: {std_dev}")
-    print(f"Median: {median}")
+    print(f'Count: {n}')
+    print(f'Min: {minimum}')
+    print(f'Max: {maximum}')
+    print(f'Mean: {mean}')
+    print(f'Standard Deviation: {std_dev}')
+    print(f'Median: {median}')
 
-if __name__ == "__main__": main()
+if __name__ == '__main__': main()
 
 # Command line = python3 22stats.py '__ __ __ __ __ __'
