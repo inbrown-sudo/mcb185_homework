7e0c8db69ba65b33ef4d55d18e0b94877ddf6e0e
inbrown-sudo <inbrown@ucdavis.edu>
2026-02-12 15:44:22 -0800

    update

--- 23birthday.py
+++ 23birthday.py
@@ -21,9 +21,29 @@ for _ in range(trials):
             break
 
 probability = duplicate_count / trials * 100
-print(f"{probability:.2f}%")
+print(f'{probability:.2f}%') 
 
 # Command line = python3 23birthday.py 10000 365 23
 # Output should be a little over 50%
 
-# Not git pushed yet 
+import sys
+import random
+
+people = int(sys.argv[1])
+calendar = int(sys.argv[2])
+iterations = int(sys.argv[3])
+
+sames = 0
+for _ in range(iterations):
+    classroom = []
+    same_birthday = False
+    for i in range(people):
+        birthday = random.randint(0, calendar-1)
+        if birthday in classroom:
+            same_birthday = True
+            break
+        classroom.append(birthday)
+    if same_birthday: sames += 1
+
+print(sames/iterations)
+	
