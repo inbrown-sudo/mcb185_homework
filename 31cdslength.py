5c50565512d23f3318426d93a014e4f627c97b11
inbrown-sudo <inbrown@ucdavis.edu>
2026-02-27 16:33:46 -0800

    update

---
+++ 31cdslength.py
@@ -0,0 +1,24 @@
+import gzip
+import sys
+
+with gzip.open(sys.argv[1], 'rt') as fp:
+    for line in fp:
+        if line[0] != '#':
+            words = line.split()
+            if words[2] == 'CDS':
+                beg = int(words[3])
+                end = int(words[4])
+                print(end - beg + 1)
+
+# Does same as above but different 
+
+with gzip.open(sys.argv[1], 'rt') as fp:
+    for line in fp:
+        if line[0] == '#': continue
+        words = line.split()
+        if words[2] != 'CDS': continue
+        beg = int(words[3])
+        end = int(words[4])
+        print(end - beg + 1)
+        
+
