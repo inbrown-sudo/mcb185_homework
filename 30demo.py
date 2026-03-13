6e20d3c2ec3c38c8ac57f1fadf67bb8017e522cd
inbrown-sudo <inbrown@ucdavis.edu>
2026-03-12 20:33:47 -0700

    update

--- 30demo.py
+++ 30demo.py
@@ -78,6 +78,83 @@ import itertools
 for nts in itertools.product('ACGT', repeat=2):
     print(nts)
     
+# Multiple Dimensions
+
+print(sys.argv)
+print(sys.argv[0])
+print(sys.argv[0][3])
+
+d = [
+    'hello',
+    (3.14, 'pi'),
+    [-1, 0, 1],
+    {'year': 2000, 'month': 7}
+]
+print(d[0][4], d[1][0], d[2][2], d[3]['month'])
+
+# Records
+
+oligo = {
+    'Name': 'SO116',
+    'Length': 18,
+    'Sequence': 'ATTTAGGTGACACTATAG',
+    'Description': 'SP6 promoter sequencing primer'
+}
+
+catalog = []
+catalog.append(oligo)
+
+def read_catalog(filepath):
+	catalog = []
+	with open(filepath) as fp:
+		for line in fp:
+			if line.startswith('#'): continue
+			name, length, seq, desc = line.rstrip().split(',')
+			record = {
+				'Name': name,
+				'Length': length,
+				'Sequence': seq,
+				'Description': desc
+			}
+			catalog.append(record)
+	return catalog
+
+catalog = read_catalog('primers.csv')
+for primer in catalog:
+    print(primer['Name'], primer['Description'])
+    
+# Dicts of Lists
+
+kcount = {}
+for i in range(len(seq) -k +1):
+	kmer = seq[i:i+k]
+	if kmer not in kcount: kcount[kmer] = 0
+	kcount[kmer] += 1
+
+seq = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGT'
+k = 2
+kloc = {}
+for i in range(len(seq) -k +1):
+	kmer = seq[i:i+k]
+	if kmer not in kloc: kloc[kmer] = []
+	kloc[kmer].append(i)
+print(kloc)
+
+#Complex Data
+# zless ~/Code/MCB185/data/GCF_000005845.2_ASM584v2_genomic.gbff.gz
+
+# JSON
+
+truc = {
+    'animals': {'dog': 'woof', 'cat': 'meow', 'pig': 'oink'},
+    'numbers': [1.09, 2.72, 3.14],
+    'is_complete': False,
+}
+print(json.dumps(truc, indent=4))
+
+
+
+
 
     
 
